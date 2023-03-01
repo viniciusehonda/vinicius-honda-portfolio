01ac59106a61335bab43af011f15d42c80c23c82
Vinicius Eidi Honda <viniciusehonda@gmail.com>
2023-02-28 23:09:26 -0300

    language selector, added contacts

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,37 @@
+const en = {
+    "profile-description": "Full Stack Developer",
+    "about": "About",
+    "skills": "Skills",
+    "contact": "Contact",
+    "name": "Name",
+    "age": "Age",
+    "country": "Country",
+    "city": "City",
+    "english": "English",
+    "portuguese": "Portuguese",
+    "brazil": "Brazil"
+};
+
+const ptBR = {
+    "profile-description": "Desenvolvedor Full Stack",
+    "about": "Sobre",
+    "skills": "Habilidades",
+    "contact": "Contato",
+    "name": "Nome",
+    "age": "Idade",
+    "country": "País",
+    "city": "Cidade",
+    "english": "Inglês",
+    "portuguese": "Português",
+    "brazil": "Brasil"
+};
+
 function Initialize() {
-    
+
     let ageElement = document.getElementById("age-text");
     ageElement.innerText = getMyAge();
+
+    initializeLanguage();
 }
 
 function getMyAge() {
@@ -9,4 +39,67 @@ function getMyAge() {
     var diff = Date.now() - myBDay.getTime();
     var age = new Date(diff);
     return Math.abs(age.getUTCFullYear() - 1970);
+}
+
+function initializeLanguage() {
+    let currentLang = getLanguage();
+
+    let languageRes = en;
+
+    changeLanguageButton(currentLang);
+
+    if (currentLang == "pt-BR") {
+        languageRes = ptBR;
+    }
+
+    document.getElementById("en-button").innerText = languageRes["english"];
+    document.getElementById("pt-button").innerText = languageRes["portuguese"];
+
+    document.getElementById("country-text").innerText = languageRes["brazil"];
+
+    document.getElementById("profile-description").innerText = languageRes["profile-description"];
+    document.getElementById("about-menu").innerText = languageRes["about"];
+    document.getElementById("skills-menu").innerText = languageRes["skills"];
+    document.getElementById("contact-menu").innerText = languageRes["contact"];
+    document.getElementById("about-title").innerText = languageRes["about"];
+    document.getElementById("skills-title").innerText = languageRes["skills"];
+    document.getElementById("contact-title").innerText = languageRes["contact"];
+
+    document.getElementById("name-label").innerText = languageRes["name"] + ":";
+    document.getElementById("age-label").innerText = languageRes["age"] + ":";
+    document.getElementById("country-label").innerText = languageRes["country"] + ":";
+    document.getElementById("city-label").innerText = languageRes["city"] + ":";
+}
+
+function getLanguage() {
+    let lang = window.localStorage.getItem("lang");
+
+    if (lang == null || lang == "") {
+        return "en";
+    }
+
+    return lang;
+}
+
+function setLanguage(language) {
+
+    changeLanguageButton();
+    window.localStorage.setItem("lang", language);
+    initializeLanguage();
+}
+
+function changeLanguageButton(language) {
+
+    if (language == "en") {
+        if (!document.getElementById("en-button").classList.contains("selected")) {
+            document.getElementById("en-button").classList.add("selected");
+            document.getElementById("pt-button").classList.remove("selected");
+        }
+    }
+    else if (language == "pt-BR") {
+        if (!document.getElementById("pt-button").classList.contains("selected")) {
+            document.getElementById("en-button").classList.remove("selected");
+            document.getElementById("pt-button").classList.add("selected");
+        }
+    }
 }
